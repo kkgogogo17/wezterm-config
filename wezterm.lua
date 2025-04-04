4e55f9d4689ddf02fa8515622caad2817dc2b027
Eric <kang.zi@northeastern.edu>
2025-04-03 21:46:45 -0400

    add gruvbox colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -1,38 +1,69 @@
---Pull in the wezterm API
+--pull in the wezterm api
 local wezterm = require("wezterm")
 local config = {}
--- This will hold the configuration
+-- this will hold the configuration
 -- local config = wezterm.config_builder()
 local mux = wezterm.mux
-local act = wezterm.action
-local Catppuccin = require("./theme/Catppuccin")
-local everforest = require("./theme/everforest")
 
--- Maxmimize the window right away
+-- maxmimize the window right away
 wezterm.on("gui-startup", function()
 	local tab, pane, window = mux.spawn_window({})
 	window:gui_window():maxmimize()
 end)
 
---This is where you actually apply your config choices
+--this is where you actually apply your config choices
 -- font size
 config.font_size = 16
 config.font = wezterm.font("JetBrains Mono")
 
--- For example, changing the colorscheme
--- config.color_scheme = "Catppuccin Frappe"
-config.colors = everforest.colors
+-- for example, changing the colorscheme
+config.color_scheme = "Gruvbox Dark (Gogh)"
+-- config.color_scheme = "mariana"
 config.adjust_window_size_when_changing_font_size = false
 
+-- Leader is the same as my old tmux prefix
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	{ key = "l", mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
-	{ key = "h", mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
-	{ key = "l", mods = "CMD", action = act.ActivatePaneDirection("Right") },
-	{ key = "h", mods = "CMD", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "CMD", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "CMD", action = act.ActivatePaneDirection("Up") },
-	{ key = "f", mods = "CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "f",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "d",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- navigating
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	{
+		mods = "LEADER",
+		key = "m",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+
+	-- rotate panes
+	{
+		mods = "LEADER",
+		key = "Space",
+		action = wezterm.action.RotatePanes("Clockwise"),
+	},
+	-- show the pane selection mode, but have it swap the active and selected panes
+	{
+		mods = "LEADER",
+		key = "0",
+		action = wezterm.action.PaneSelect({
+			mode = "SwapWithActive",
+		}),
+	},
 }
 
 config.window_background_opacity = 0.9
